9809dfa3dbf1352b4eb60ac45a43fab00c326bbf
sizatn <sizatn2013@gmail.com>
2017-12-20 11:09:54 +0800

    first commit

---
+++ src/main/java/com/sizatn/springbootredis/configuration/RedisConfig.java
@@ -0,0 +1,45 @@
+package com.sizatn.springbootredis.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import redis.clients.jedis.JedisPoolConfig;
+
+@Configuration
+public class RedisConfig {
+
+	@Autowired
+	private RedisProperties redisProperties;
+
+	@Bean
+	public JedisPoolConfig jedisPoolConfig() {
+		JedisPoolConfig jpc = new JedisPoolConfig();
+		jpc.setMaxIdle(redisProperties.getPool().getMaxIdle());
+		jpc.setMinIdle(redisProperties.getPool().getMinIdle());
+		jpc.setMaxWaitMillis(redisProperties.getPool().getMaxWait());
+		jpc.setMaxTotal(redisProperties.getPool().getMaxActive());
+		return jpc;
+	}
+
+	@Bean
+	public JedisConnectionFactory jedisConnectionFactory() {
+		JedisConnectionFactory jcf = new JedisConnectionFactory();
+		jcf.setDatabase(redisProperties.getDatabase());
+		jcf.setHostName(redisProperties.getHost());
+		jcf.setPort(redisProperties.getPort());
+		jcf.setPoolConfig(jedisPoolConfig());
+		return jcf;
+	}
+
+	@Bean
+	public StringRedisTemplate redisTemplate() {
+		StringRedisTemplate srt = new StringRedisTemplate();
+		srt.setConnectionFactory(jedisConnectionFactory());
+		return srt;
+	}
+
+}
